219d8f50f938295c67b7af9401951a7ee9f555ab
ArthurKouam <164850622+ArthurKouam@users.noreply.github.com>
2024-08-09 06:07:01 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="w-full grid grid-cols-1 lg:grid lg:h-screen lg:grid-cols-2 bg-neutral-800 lg:place-items-center">
+    <section class="w-full grid gap-2 h-max-content grid-cols-1 lg:h-screen lg:grid-cols-2 bg-neutral-800 lg:place-items-center">
 
         <div class="lg:h-full h-screen grid pl-7 pt-16">
             <div class="h-full">
@@ -8,8 +8,7 @@
                 </div>
                 <div class="pt-5 w-3/4 text-justify" data-aos="slide-up">
                     <h2 class="text-2xl text-white font-bold">ABOUT ME</h2>
-                    <p class="pt-3 text-white h-full">
-                        Passionate about web development, I am a junior developer with a strong interest in front-end technologies like Javascript and Vue.js. I have already completed several projects, including a mobile application developed with Quasar. Curious and always learning, I continuously expand my skills by exploring modern frameworks and tools. My goal is to contribute to projects that combine innovation and performance while delivering smooth and intuitive user experiences.
+                    <p class="pt-3 text-white h-full">Passionate about web development, I am a junior developer with a strong intérêt in web technologies like Javascript and Vue.js. I have already completed several projects, including a mobile application developed with Quasar. Curious and always learning, I continuously expand my skills by exploring modern frameworks and tools. My goal is to contribute to projects that combine innovation and performance while delivering smooth and intuitive user experiences.
                     </p>
                 </div>
             </div>
