f82c445f2665d6d7c6d3afdea361a3a7dacc51e6
Arthur Kouam <164850622+ArthurKouam@users.noreply.github.com>
2024-11-06 12:18:03 +0100

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
                 <div class="w-3/4 lg:h-3/5 h-40v bg-gray-800 rounded-2xl" data-aos="zoom-in">
 
                 </div>
-                <div class="pt-5 pb-3 lg:w-3/4 w-full overflow-x-hidden" data-aos="slide-up">
+                <div class="pt-5 pb-3 lg:w-3/4 w-full overflow-x-hidden" data-aos="slide-left">
                     <h2 class="text-2xl text-white font-bold">ABOUT ME</h2>
                     <p class="pt-3 text-white text-justify pr-3">Passionate about web development, I am a junior developer with a strong interest in web technologies like Javascript and Vue.js. I have already completed several projects. Curious and always learning, I continuously expand my skills by exploring modern frameworks and tools. My goal is to contribute to projects that combine innovation and performance while delivering smooth and intuitive user experiences.
                     </p>
@@ -14,7 +14,7 @@
             </div>
         </div>
 
-        <div class="pb-5 pt-6 lg:pt-0 lg:pb-0 pl-8 lg:pl-0 lg:h-full flex items-center" data-aos="slide-up">
+        <div class="pb-5 pt-6 lg:pt-0 lg:pb-0 pl-8 lg:pl-0 lg:h-full flex items-center" data-aos="slide-rigth">
             <div class="text-white">
                 <h1 class="text-white text-3xl lg:text-4xl font-bold">MY BACKGROUND SCHOOL</h1>
                 <p class="mt-5"><span class="border-2 rounded-full px-3">2023...</span><span class="text-xl">Computer engineering students at IUT FV</span></p>
